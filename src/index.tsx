edd732e42692fd9472eb48e5d9c5036c6c80761a
velomovies <victor@victorzumpolle.nl>
2021-12-20 11:50:02 +0100

    Add migrateFields lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import {
   connect,
+  OnBootCtx,
   RenderFieldExtensionCtx,
   RenderManualFieldExtensionConfigScreenCtx,
 } from 'datocms-plugin-sdk'
 import { render } from './utils/render'
+import migrateFields from './lib/migrateFields'
+
 import ConfigScreen from './entrypoints/ConfigScreen'
 import FieldExtension from './entrypoints/FieldExtension'
 import FieldExtensionConfigScreen from './entrypoints/FieldExtensionConfigScreen'
@@ -11,20 +14,23 @@ import FieldExtensionConfigScreen from './entrypoints/FieldExtensionConfigScreen
 import 'datocms-react-ui/styles.css'
 import './styles/index.css'
 
+const fieldSettings = {
+  id: 'jsonTable',
+  name: 'JSON table',
+}
+
 connect({
   renderConfigScreen(ctx) {
     return render(<ConfigScreen ctx={ctx} />)
   },
   manualFieldExtensions() {
-    return [
-      {
-        id: 'jsonTable',
-        name: 'JSON table',
-        type: 'editor',
-        fieldTypes: ['json'],
-        configurable: true,
-      },
-    ]
+    return [{
+      id: fieldSettings.id,
+      name: fieldSettings.name,
+      type: 'editor',
+      fieldTypes: ['json'],
+      configurable: true,
+    }]
   },
   renderManualFieldExtensionConfigScreen(
     _,
@@ -34,5 +40,8 @@ connect({
   },
   renderFieldExtension(_, ctx: RenderFieldExtensionCtx) {
     return render(<FieldExtension ctx={ctx} />)
+  },
+  async onBoot(ctx: OnBootCtx) {
+    await migrateFields(ctx, fieldSettings.id)
   },
 })
